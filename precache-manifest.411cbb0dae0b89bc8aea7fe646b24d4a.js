3a6dab7fa3da734bdb663c6f79e00840f9a0776f
Cooper Tuch <coopertuch@gmail.com>
2019-11-22 13:15:57 -0800

    Updates

--- precache-manifest.411cbb0dae0b89bc8aea7fe646b24d4a.js
+++ precache-manifest.411cbb0dae0b89bc8aea7fe646b24d4a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d18af4ea967c39c0ef24366512736abb",
+    "revision": "4fb48ae3e422537e8da1a1ce3d4576f9",
     "url": "/Portfolio/index.html"
   },
   {
-    "revision": "2f236291880f54250f88",
+    "revision": "eea9868d65668ed6310c",
     "url": "/Portfolio/static/css/main.b954356f.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio/static/js/2.9faa3214.chunk.js"
   },
   {
-    "revision": "2f236291880f54250f88",
-    "url": "/Portfolio/static/js/main.17d69cd4.chunk.js"
+    "revision": "eea9868d65668ed6310c",
+    "url": "/Portfolio/static/js/main.a44e1f85.chunk.js"
   },
   {
     "revision": "63fe68486ecb758e5a56",

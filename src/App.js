78d1e1ca6262fab1a04481c4e0acef4120100e84
Cooper Tuch <coopertuch@gmail.com>
2019-12-19 18:10:53 -0800

    fixing 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Route, Link, Switch, Redirect } from "react-router-dom";
+import { Route, Link, Switch, Redirect, Router } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Blog from "./components/Blog";
@@ -9,34 +9,36 @@ import Music from "./components/Music";
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <header className="header">
-          <nav>
-            <Link className="link" to="/Portfolio">
-              <h1 className="link"> Home </h1>
-            </Link>
-            <Link className="link" to="/projects">
-              <h1 className="link"> Projects </h1>
-            </Link>
-            <Link className="link" to="/blog">
-              <h1 className="link"> Blog </h1>
-            </Link>
-            <Link className="link" to="/music">
-              <h1 className="link"> Music </h1>
-            </Link>
-          </nav>
-        </header>
+      <Router>
+        <div className="app">
+          <header className="header">
+            <nav>
+              <Link className="link" to="/Portfolio">
+                <h1 className="link"> Home </h1>
+              </Link>
+              <Link className="link" to="/projects">
+                <h1 className="link"> Projects </h1>
+              </Link>
+              <Link className="link" to="/blog">
+                <h1 className="link"> Blog </h1>
+              </Link>
+              <Link className="link" to="/music">
+                <h1 className="link"> Music </h1>
+              </Link>
+            </nav>
+          </header>
 
-        <main>
-          <Switch>
-            <Route path="/Portfolio" exact component={Home} />
-            <Route path="/projects" exact component={Projects} />
-            <Route path="/blog" exact component={Blog} />
-            <Route path="/music" exact component={Music} />
-            <Redirect from="/*" to="/Portfolio" />
-          </Switch>
-        </main>
-      </div>
+          <main>
+            <Switch>
+              <Route path="/Portfolio" exact component={Home} />
+              <Route path="/projects" exact component={Projects} />
+              <Route path="/blog" exact component={Blog} />
+              <Route path="/music" exact component={Music} />
+              <Redirect from="/*" to="/Portfolio" />
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }

20ae76d3ef12c5cc7b18f30fea74e6f5ea6df196
Cooper Tuch <coopertuch@gmail.com>
2019-12-27 10:31:47 -0800

    project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,24 +19,6 @@ class Projects extends Component {
         </h2> */}
         <div className="project">
           <span>
-            {/* <h3>
-              <a
-                className="url"
-                href="https://we-work-out.herokuapp.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                [WeWorkout]{" "}
-              </a>
-              <a
-                className="url"
-                href="https://github.com/coopertuch/GA-Project-4"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                [GitHub]
-              </a>
-            </h3> */}
             <p className="body">
               <a
                 className="url"
@@ -65,7 +47,7 @@ class Projects extends Component {
               to be logged in to just view workouts and lifts. Check out the
               GitHub repository{" "}
               <a
-                className="url"
+                className="repo"
                 href="https://github.com/coopertuch/GA-Project-4"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -78,7 +60,7 @@ class Projects extends Component {
         </div>
         <div className="project">
           <span>
-            <h3>
+            <p className="body">
               <a
                 className="url"
                 href="https://coopertuch.github.io/GA-Project-1/"
@@ -86,24 +68,14 @@ class Projects extends Component {
                 rel="noopener noreferrer"
               >
                 [BB'z Questions]{" "}
-              </a>
-              <a
-                className="url"
-                href="https://github.com/coopertuch/GA-Project-1"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                [GitHub]
-              </a>
-            </h3>
-            <p className="body">
-              BB'z Questions is a collection of questions split into several
-              various categories, built using HTML, CSS, and Javascript. This
-              project is intended for use by new friends, old friends, family,
-              anyone you hope to get closer to or have some fun with. So often
-              we are paralyzed by social interactions and overthinking that we
-              don't even begin to converse with each other, always resorting to
-              small talk; I've been doing well, the weather is good, so was my
+              </a>{" "}
+              is a collection of questions split into several various
+              categories, built using HTML, CSS, and Javascript. This project is
+              intended for use by new friends, old friends, family, anyone you
+              hope to get closer to or have some fun with. So often we are
+              paralyzed by social interactions and overthinking that we don't
+              even begin to converse with each other, always resorting to small
+              talk; I've been doing well, the weather is good, so was my
               weekend. While you may not want to get into personal details with
               co-workers, this project offers a variety of (growing) categories
               and questions to spark up conversations with each other; spend
@@ -114,12 +86,22 @@ class Projects extends Component {
               Sometimes all it takes is one question to start meaningful
               conversations and open the door to connection. A huge thank you to
               the Banana Boyz, or "BB'z" for short, for inspiring this app idea!
+              Check out the GitHub repository{" "}
+              <a
+                className="repo"
+                href="https://github.com/coopertuch/GA-Project-1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                here
+              </a>
+              .
             </p>
           </span>
         </div>
         <div className="project">
           <span>
-            <h3>
+            <p className="body">
               <a
                 className="url"
                 href="https://coopscheerups.herokuapp.com/"
@@ -127,34 +109,32 @@ class Projects extends Component {
                 rel="noopener noreferrer"
               >
                 [Coop's Cheerups]{" "}
-              </a>
+              </a>{" "}
+              is a board of positive quotes, built using HTML, CSS, Javascript,
+              Express, Handlebars, and Mongoose. Coop's Cheerups is deployed to
+              Heroku, and is responsive for desktop and mobile. On load, all
+              Cheerups will be displayed in single rows, or rows of one to
+              three, responsive to your browser. Underneath the logo, there is a
+              "New Cheerup" button which directs users to a new page where they
+              can create their own cheerup; on this page, they will be required
+              to enter the quote and author, with a maximum of 100 and 20
+              characters for the quote and author, respectively. After
+              submitting a new cheerup, they will be redirected back to the home
+              page. Each cheerup has the option to edit or remove; the edit will
+              direct the user to a new page that has pulled the new cheerup and
+              allows users to edit the quote, author, and update likes. After
+              submitting an edit, they will be redirected back to the home page.
+              The remove button will instantly delete the quote. Check out the
+              GitHub repository{" "}
               <a
-                className="url"
+                className="repo"
                 href="https://github.com/coopertuch/GA-Project-2"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                [GitHub]
+                here
               </a>
-            </h3>
-            <p className="body">
-              When you need a little positivity in your life, look no further
-              than Cheerups. Coop's Cheerups is a board of positive quotes,
-              built using HTML, CSS, Javascript, Express, Handlebars, and
-              Mongoose. Coop's Cheerups is deployed to Heroku, and is responsive
-              for desktop and mobile. On load, all Cheerups will be displayed in
-              single rows, or rows of one to three, responsive to your browser.
-              Underneath the logo, there is a "New Cheerup" button which directs
-              users to a new page where they can create their own cheerup; on
-              this page, they will be required to enter the quote and author,
-              with a maximum of 100 and 20 characters for the quote and author,
-              respectively. After submitting a new cheerup, they will be
-              redirected back to the home page. Each cheerup has the option to
-              edit or remove; the edit will direct the user to a new page that
-              has pulled the new cheerup and allows users to edit the quote,
-              author, and update likes. After submitting an edit, they will be
-              redirected back to the home page. The remove button will instantly
-              delete the quote.
+              .
             </p>
           </span>
         </div>

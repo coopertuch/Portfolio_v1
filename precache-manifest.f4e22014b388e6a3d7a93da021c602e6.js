2c247d70f844efdecdb39969726fff0d01fae328
Cooper Tuch <coopertuch@gmail.com>
2019-12-27 11:11:16 -0800

    Updates

---
+++ precache-manifest.f4e22014b388e6a3d7a93da021c602e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6505c9d45bd3150fb071a6ea1ff74f64",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "cab872db1e2b5125fb95",
+    "url": "/Portfolio/static/css/main.7707f561.chunk.css"
+  },
+  {
+    "revision": "bb4dc80220626966dabf",
+    "url": "/Portfolio/static/js/2.addd773a.chunk.js"
+  },
+  {
+    "revision": "cab872db1e2b5125fb95",
+    "url": "/Portfolio/static/js/main.8b7d8ca4.chunk.js"
+  },
+  {
+    "revision": "63fe68486ecb758e5a56",
+    "url": "/Portfolio/static/js/runtime-main.b0957d76.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.e8bbee934067ce57797828b5bf8a7df2.js"
+  "/Portfolio/precache-manifest.f4e22014b388e6a3d7a93da021c602e6.js"
 );
 
 self.addEventListener('message', (event) => {
